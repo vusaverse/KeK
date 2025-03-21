18ed680ec1391be5b261c034bf465cddca002406
tin900 <113692500+tin900@users.noreply.github.com>
2025-03-21 12:46:52 +0100

    update: documentation system variable

--- 00_project/02_load_systemvairables.R
+++ 00_project/02_load_systemvairables.R
@@ -30,6 +30,13 @@ pmap(list(to_set$variable, to_set$value), set_all_envs)
 ## Set SHAREPOINT_DIR
 Sys.setenv(SHAREPOINt_DIR = paste0("C:/Users/", Sys.getenv("USERNAME"), "/Vrije Universiteit Amsterdam/"))
 
+
+Sys.setenv(DOCUMENTATION_DIR =
+             paste0(Sys.getenv("SHAREPOINt_DIR"), "Education Analytics - General/01_Documentatie/XX. Data Documentatie/"))
+
+Sys.setenv(MAP_TABLE_DIR =
+             paste0(Sys.getenv("SHAREPOINt_DIR"), "Education Analytics - General/01_Documentatie/XX. Data Documentatie/Mapping Tables/"))
+
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## RUIM OP ####
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++

33c8191b91ff67bf3a474aba6c2b63054b5fa191
tkr231 <t.s.koster@vu.nl>
2026-04-17 16:20:59 +0200

    feat: start replacing with uas

--- 01_read/Export Vak.R
+++ 01_read/Export Vak.R
@@ -12,18 +12,36 @@
 
 dfVAKAS <- readrds_csv(output = "3. Analyseset/Vakken_Analyseset_lang_na_stap_2.rds")
 
-##' Needed to determine INS_Opleidingscode from RES_Studieprogramma_ID
-dfOpleidingskoppel <- readrds_csv(output = "2. Geprepareerde data/INS_Opleidingkoppel.rds") %>% 
-  select(RES_Studieprogramma_ID, 
-         INS_Opleidingscode_actueel
+dfuas_vakken <- load_rds_from_azure(
+  config = .config,
+  container_type = "bronze",
+  file_name = "UAS_Vakken_enriched.rds"
+)
+
+dfuas_groepen <- load_rds_from_azure(
+  config = .config,
+  container_type = "bronze",
+  file_name = "UAS_Groepen_van_vakken_enriched.rds"
+) %>% 
+  select(
+    uas_groep_jaar,
+    code_mg,
+    verplichte_keuze
   ) %>% 
   distinct()
+
+##' Needed to determine INS_Opleidingscode from RES_Studieprogramma_ID
+# dfOpleidingskoppel <- readrds_csv(output = "2. Geprepareerde data/INS_Opleidingkoppel.rds") %>% 
+#   select(RES_Studieprogramma_ID, 
+#          INS_Opleidingscode_actueel
+#   ) %>% 
+#   distinct()
 
 dfDates <- readrds_csv(output = "2. Geprepareerde data/ACA_Dates.rds")
 
 ## validation purposes
-dfTrue <- readxl::read_xlsx(paste0(Sys.getenv("RAW_DATA_DIR"), "/KeK/Overzicht Vak 6_8_2024 15-30-41.xlsx")) %>%
-  select(-Eigenaar)
+# dfTrue <- readxl::read_xlsx(paste0(Sys.getenv("RAW_DATA_DIR"), "/KeK/Overzicht Vak 6_8_2024 15-30-41.xlsx")) %>%
+#   select(-Eigenaar)
 
 dfDates_join <- dfDates %>%
   filter(!is.na(ACA_Periode)) %>%
@@ -68,39 +86,87 @@ dfResultaten_koppeling <- dfResultaten %>%
 ## 2. BEWERKEN ####
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-dfVAK <- dfVAKAS %>%
-  filter(UAS_Vak_Jaar >= 2021) %>%
+## Determine period start. Previously done in sa-scripts, not in ea-repo yet.
+## TODO remove if logic is added to ETL in EA
+dfuas_vakken <- dfuas_vakken  %>%
+  ## Kies bij vakken met met meerdere periode(blokken) voor de laatste periode
+  ## Dit zijn meestal scripties die ook in eerste semester kunnen worden
+  ## gevolgd, maar in curriculum in tweede semester thuis horen.
+  mutate(
+    uas_aangeboden_periodes = case_when(
+      str_detect(uas_aangeboden_periodes, ",") ~ str_split_i(uas_aangeboden_periodes, ", ", i = -1),
+      .default = uas_aangeboden_periodes
+    )
+  ) %>%
+  ## Transformeer nu alle waardes naar periodes of NA
+  mutate(
+    uas_aangeboden_periodes = case_when(
+      uas_aangeboden_periodes == "Semester 1" ~ "Periode 1+2+3",
+      uas_aangeboden_periodes == "Semester 2" ~ "Periode 4+5+6",
+      uas_aangeboden_periodes == "Zomerperiode" ~ "Periode 7",
+      uas_aangeboden_periodes == "025" ~ NA_character_,
+      ## Ook hiervoor geldt dat het vaak scripties zijn
+      uas_aangeboden_periodes == "Ac. Jaar (september)" &
+        uas_studiepunten >= 54 ~ "Periode 1+2+3+4+5+6",
+      uas_aangeboden_periodes == "Ac. Jaar (september)" &
+        uas_studiepunten >= 42 ~ "Periode 2+3+4+5+6",
+      uas_aangeboden_periodes == "Ac. Jaar (september)" &
+        uas_studiepunten >= 36 ~ "Periode 3+4+5+6",
+      uas_aangeboden_periodes == "Ac. Jaar (september)" &
+        uas_studiepunten >= 24 ~ "Periode 4+5+6",
+      uas_aangeboden_periodes == "Ac. Jaar (september)" &
+        uas_studiepunten >= 9 ~ "Periode 5+6",
+      ## Dit zijn vaak mentor / tutoraat vakken
+      uas_aangeboden_periodes == "Ac. Jaar (september)" ~ "Periode 1+2+3+4+5+6",
+      .default = uas_aangeboden_periodes
+    ),
+    ## Zet periodes van vak in een lijst
+    uas_aangeboden_periodes_lijst = str_extract_all(uas_aangeboden_periodes, "[0-9]"),
+    ## Bepaal eerste periode vak
+    uas_vak_periode_start = parse_number(uas_aangeboden_periodes)
+  ) %>%
+  rowwise() %>%
+  mutate(
+    ## Bepaal duratie van vak
+    uas_vak_duur = sum(str_count(uas_aangeboden_periodes, "[+]"),
+                       1,
+                       na.rm = TRUE
+    )
+  ) %>%
+  ungroup() %>%
+  distinct()
+
+
+dfVAK <- dfuas_vakken %>%
+  filter(uas_vak_jaar >= 2021) %>%
   left_join(dfDates_join, by = c(
-    "UAS_Vak_Jaar" = "INS_Inschrijvingsjaar",
-    "UAS_Vak_Periode_start" = "ACA_Periode"
+    "uas_vak_jaar" = "INS_Inschrijvingsjaar",
+    "uas_vak_periode_start" = "ACA_Periode"
   )) %>%
   left_join(dfResultaten_koppeling, by = c(
-    "UAS_Vak_Jaar" = "Academisch_jaar",
-    "UAS_Vak_Code" = "RES_Module_code"
+    "uas_vak_jaar" = "Academisch_jaar",
+    "uas_code" = "RES_Module_code"
   )) %>%
-  rename(Startmoment_vak = ACA_Einddatum_prev) %>%
+  rename(startmoment_vak = ACA_Einddatum_prev) %>%
   ## Merge insrtuctie taal kolommen
-  mutate(OPL_Instructietaal = case_when(
-    is.na(OPL_Instructietaal) ~
-      case_when(
-        UAS_Opleiding_Onderwijstaal == "Engels (EN)" ~ "Engels",
-        UAS_Opleiding_Onderwijstaal == "Nederlands (NL)" ~ "Nederlands",
-        UAS_Opleiding_Onderwijstaal == "Tweetalig (Z1)" ~ "Nederlands/Engels",
-        TRUE ~ UAS_Opleiding_Onderwijstaal
-      ),
-    TRUE ~ OPL_Instructietaal
-  )) %>%
+  mutate(uas_onderwijstaal = case_when(
+          uas_onderwijstaal == "Engels (EN)" ~ "Engels",
+          uas_onderwijstaal == "Nederlands (NL)" ~ "Nederlands",
+          uas_onderwijstaal == "Tweetalig (Z1)" ~ "Nederlands/Engels",
+          .default = uas_onderwijstaal
+      )
+  ) %>%
   select(
-    UAS_Vak_Code, # KeK Opleidingscode ------------- NVT
-    UAS_Vak_Jaar, # Jaargang
-    UAS_Vak_Naam_NL, # Vak Naam
-    UAS_Vak_Studiepunten, # EC's; verder hebben wij ook minimum en maximum EC
-    UAS_Groep_Verplichte_keuze, # Verplicht/Keuze; niet helemaal zeker
-    UAS_Vak_Periode_start, Startmoment_vak, # Startdatum
-    UAS_Vak_Duur, # Aantal Collegeweken
+    uas_code, # KeK Opleidingscode ------------- NVT
+    uas_vak_jaar, # Jaargang
+    uas_vaknaam_nl, # Vak Naam
+    uas_studiepunten, # EC's; verder hebben wij ook minimum en maximum EC
+    uas_groep_verplichte_keuze, # Verplicht/Keuze; niet helemaal zeker
+    uas_vak_periode_start, startmoment_vak, # Startdatum
+    uas_vak_duur, # Aantal Collegeweken
     total_students, # Aantal Deelnemers
-    OPL_Instructietaal, UAS_Opleiding_Onderwijstaal, # Taal; zie ook OPLAS repo om dit te mergen?
-    RES_Studieprogramma_ID,
+    uas_onderwijstaal, # Taal; zie ook OPLAS repo om dit te mergen?
+    uas_extern_id,
     # Hoorcollege Aantal personen WP
     # Hoorcollege Aantal personen OBP
     # Werkcollege Aantal personen WP
@@ -112,10 +178,10 @@ dfVAK <- dfVAKAS %>%
     # Werkvorm 2 Aantal personen WP
     # Werkvorm 2 Aantal personen OBP
     
-    UAS_Groep_Groepstype,
-    UAS_Groep_College_jaar,
-    UAS_Vak_Opleidingsnaam_eigenaar,
-    UAS_Vak_Opleidingscode_eigenaar,
+    uas_groep_groepstype,
+    uas_groep_college_jaar,
+    uas_vak_opleidingsnaam_eigenaar,
+    uas_vak_opleidingscode_eigenaar,
     ## TERMTIME?
     # Werkvorm 3 Naam
     # Werkvorm 3 Groepen
@@ -134,41 +200,41 @@ dfVAK <- dfVAKAS %>%
     count_with_result,
     count_afgesloten # Aantal geslaagden vak
   ) %>%
-  filter(!is.na(UAS_Vak_Naam_NL)) %>%
+  #filter(!is.na(uas_vaknaam_nl)) %>%
   distinct() %>%
   ## bij dubbelingen missende waarden wellicht nog wat trucjes nodig, zie E_IBA1_ACC voor >= 2022
-  distinct(UAS_Vak_Code, UAS_Vak_Jaar, UAS_Vak_Periode_start, Startmoment_vak, .keep_all = TRUE) %>%
-  mutate(UAS_Vak_Periode_einde = UAS_Vak_Periode_start + UAS_Vak_Duur - 1) %>%
+  distinct(uas_code, uas_vak_jaar, uas_vak_periode_start, startmoment_vak, .keep_all = TRUE) %>%
+  mutate(uas_vak_periode_einde = uas_vak_periode_start + uas_vak_Duur - 1) %>%
   left_join(dfDates_join, by = c(
-    "UAS_Vak_Jaar" = "INS_Inschrijvingsjaar",
-    "UAS_Vak_Periode_einde" = "ACA_Periode"
+    "uas_vak_jaar" = "INS_Inschrijvingsjaar",
+    "uas_vak_periode_einde" = "ACA_Periode"
   )) %>%
-  ## if UAS_Vak_Periode_start == 3 then add 14 days to startmoment_vak
-  mutate(Startmoment_vak = case_when(
-    UAS_Vak_Periode_start == 3 ~ Startmoment_vak + 14,
-    TRUE ~ Startmoment_vak
+  ## if uas_vak_periode_start == 3 then add 14 days to startmoment_vak
+  mutate(startmoment_vak = case_when(
+    uas_vak_periode_start == 3 ~ startmoment_vak + 14,
+    TRUE ~ startmoment_vak
   )) %>%
-  mutate(weeks_diff = round(as.numeric(difftime(ACA_Einddatum, Startmoment_vak, units = "weeks")))) %>% 
-  left_join(dfOpleidingskoppel, by = c("RES_Studieprogramma_ID"))
+  mutate(weeks_diff = round(as.numeric(difftime(ACA_Einddatum, startmoment_vak, units = "weeks")))) #%>% 
+  #left_join(dfOpleidingskoppel, by = c("RES_Studieprogramma_ID"))
 
 
 dfVAK_validatie <- dfVAK %>%
   filter(
-    UAS_Vak_Jaar == 2021,
-    UAS_Vak_Code %in% dfTrue$Vakcode
+    uas_vak_jaar == 2021,
+    uas_code %in% dfTrue$Vakcode
   ) %>%
-  left_join(dfTrue, by = c("UAS_Vak_Code" = "Vakcode")) %>%
+  left_join(dfTrue, by = c("uas_code" = "Vakcode")) %>%
   mutate(
-    ECs_overeen = UAS_Vak_Studiepunten == `EC's`,
+    ECs_overeen = uas_studiepunten == `EC's`,
     Startdatum_date = as_date(Startdatum) + 1,
-    Startdatum_overeen = Startdatum_date == Startmoment_vak,
-    verschil_startdatum = as.numeric(Startmoment_vak - Startdatum_date),
+    Startdatum_overeen = Startdatum_date == startmoment_vak,
+    verschil_startdatum = as.numeric(startmoment_vak - Startdatum_date),
     Aantal_deelnemers_overeen = total_students == `Aantal Deelnemers`,
     Verschil_aantal_deelnemers = total_students - `Aantal Deelnemers`,
     Aantal_weken_overeen = weeks_diff == `Aantal Collegeweken`,
     Verschil_aantal_weken = weeks_diff - `Aantal Collegeweken`,
     KEK_verplicht_boolean = `Verplicht/Keuze` == "Verplicht",
-    verplicht_overeen = UAS_Groep_Verplichte_keuze == KEK_verplicht_boolean,
+    verplicht_overeen = uas_Groep_Verplichte_keuze == KEK_verplicht_boolean,
     Aantal_geslaagden_overeen = count_afgesloten == `Aantal Geslaagden Eindtoets`,
     verschil_geslaagden = count_afgesloten - `Aantal Geslaagden Eindtoets`
   )
@@ -177,44 +243,44 @@ dfVAK_validatie <- dfVAK %>%
 ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 ## Add scriptie information
 
-dfSc <- dfVAKAS %>% filter(UAS_Vak_scriptie) %>% 
-  select(UAS_Vak_Jaar, UAS_Vak_Code, UAS_Vak_scriptie, UAS_Opleiding_Fase_minimaal_generiek, UAS_Vak_Periode_start) %>% 
-  filter(!is.na(UAS_Opleiding_Fase_minimaal_generiek)) %>%
-  ## mutate case when for new variable unl_eindtoetsnaam, if UAS_Opleiding_Fase_minimaal_generiek == Bachelor then 941790000, IF 
-  ## UAS_Opleiding_Fase_minimaal_generiek == Master then 941790001
+dfSc <- dfVAKAS %>% filter(uas_vak_scriptie) %>% 
+  select(uas_vak_jaar, uas_code, uas_vak_scriptie, uas_Opleiding_Fase_minimaal_generiek, uas_vak_periode_start) %>% 
+  filter(!is.na(uas_Opleiding_Fase_minimaal_generiek)) %>%
+  ## mutate case when for new variable unl_eindtoetsnaam, if uas_Opleiding_Fase_minimaal_generiek == Bachelor then 941790000, IF 
+  ## uas_Opleiding_Fase_minimaal_generiek == Master then 941790001
   mutate(unl_eindtoetsnaam = case_when(
-    UAS_Opleiding_Fase_minimaal_generiek == "Bachelor" ~ 941790000,
-    UAS_Opleiding_Fase_minimaal_generiek == "Master" ~ 941790001,
+    uas_Opleiding_Fase_minimaal_generiek == "Bachelor" ~ 941790000,
+    uas_Opleiding_Fase_minimaal_generiek == "Master" ~ 941790001,
     TRUE ~ NA_real_
   )) %>% 
-  select(UAS_Vak_Jaar, UAS_Vak_Code, unl_eindtoetsnaam, UAS_Vak_Periode_start) %>% 
+  select(uas_vak_jaar, uas_code, unl_eindtoetsnaam, uas_vak_periode_start) %>% 
   distinct()
 
 
 dfVAK <- dfVAK %>% 
-  left_join(dfSc, by = c("UAS_Vak_Jaar", "UAS_Vak_Code", "UAS_Vak_Periode_start")) %>% 
-  distinct(UAS_Vak_Code, UAS_Vak_Jaar, UAS_Vak_Periode_start, Startmoment_vak, .keep_all = TRUE)
+  left_join(dfSc, by = c("uas_vak_jaar", "uas_code", "uas_vak_periode_start")) %>% 
+  distinct(uas_code, uas_vak_jaar, uas_vak_periode_start, startmoment_vak, .keep_all = TRUE)
 
 ## Add stage information
-dfStage <- dfVAKAS %>% filter(UAS_Vak_stage) %>% 
-  select(UAS_Vak_Jaar, UAS_Vak_Code, UAS_Vak_stage, UAS_Opleiding_Fase_minimaal_generiek, UAS_Vak_Periode_start) %>%
-  filter(!is.na(UAS_Opleiding_Fase_minimaal_generiek)) %>%
+dfStage <- dfVAKAS %>% filter(uas_vak_stage) %>% 
+  select(uas_vak_jaar, uas_code, uas_vak_stage, uas_Opleiding_Fase_minimaal_generiek, uas_vak_periode_start) %>%
+  filter(!is.na(uas_Opleiding_Fase_minimaal_generiek)) %>%
   mutate(unl_eindtoetsnaam = 941790007) %>% 
-  select(UAS_Vak_Jaar, UAS_Vak_Code, unl_eindtoetsnaam, UAS_Vak_Periode_start) %>% 
+  select(uas_vak_jaar, uas_code, unl_eindtoetsnaam, uas_vak_periode_start) %>% 
   distinct()
 
 ##' *TODO* why reallocated values?
 dfVAK <- dfVAK %>%
   left_join(
     dfStage %>% 
-      select(UAS_Vak_Jaar, UAS_Vak_Code, UAS_Vak_Periode_start, unl_eindtoetsnaam),
-    by = c("UAS_Vak_Jaar", "UAS_Vak_Code", "UAS_Vak_Periode_start")
+      select(uas_vak_jaar, uas_code, uas_vak_periode_start, unl_eindtoetsnaam),
+    by = c("uas_vak_jaar", "uas_code", "uas_vak_periode_start")
   ) %>%
   mutate(
     unl_eindtoetsnaam = coalesce(unl_eindtoetsnaam.y, unl_eindtoetsnaam.x)
   ) %>%
   select(-unl_eindtoetsnaam.x, -unl_eindtoetsnaam.y) %>% 
-  distinct(UAS_Vak_Code, UAS_Vak_Jaar, UAS_Vak_Periode_start, Startmoment_vak, .keep_all = TRUE)
+  distinct(uas_code, uas_vak_jaar, uas_vak_periode_start, startmoment_vak, .keep_all = TRUE)
 
 
 ##' attempt to fill missing unl_eindtoetsnaam with filled dfToetsvormen variable toetsvorm_code 
@@ -229,18 +295,18 @@ dfVAK <- dfVAK %>%
   left_join(
     dfToetsvormen %>%
       select(
-        UAS_Vak_Jaar,
+        uas_vak_jaar,
         Code,
         toetsvorm_code
       ) %>%
       distinct(),
-    by = c("UAS_Vak_Jaar", "UAS_Vak_Code" = "Code")
+    by = c("uas_vak_jaar", "uas_code" = "Code")
   ) %>%
   mutate(
     unl_eindtoetsnaam = coalesce(toetsvorm_code, unl_eindtoetsnaam)
   ) %>%
   select(-toetsvorm_code) %>%
-  distinct(UAS_Vak_Code, UAS_Vak_Jaar, UAS_Vak_Periode_start, Startmoment_vak, .keep_all = TRUE)
+  distinct(uas_code, uas_vak_jaar, uas_vak_periode_start, startmoment_vak, .keep_all = TRUE)
 
 dfVAK %>% filter(is.na(unl_eindtoetsnaam)) %>% dim
 # [1] 9931   23
